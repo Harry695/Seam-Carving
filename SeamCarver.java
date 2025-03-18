9c5b443f9cfe4e8bc734af71bf5906f3516e78cc
Harry <oyjunhan@hotmail.com>
2025-03-17 20:03:52 -0700

    Implement picture(), width(), length() and data structures

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,18 +1,29 @@
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
+    Picture picture;
+    int[][] energy;
 
     // create a seam carver object based on the given picture
-    public SeamCarver(Picture picture)
+    public SeamCarver(Picture picture) {
+        this.picture = new Picture(picture);
+        energy = new int[picture.height()][picture.width()];
+    }
  
     // current picture
-    public Picture picture()
+    public Picture picture() {
+        return new Picture(picture);
+    }
  
     // width of current picture
-    public int width()
+    public int width() {
+        return picture.width();
+    }
  
     // height of current picture
-    public int height()
+    public int height() {
+        return picture.height();
+    }
  
     // energy of pixel at column x and row y
     public double energy(int x, int y)
@@ -21,7 +32,7 @@ public class SeamCarver {
     public int[] findHorizontalSeam()
  
     // sequence of indices for vertical seam
-    public int[] findVerticalSeam()
+    public int[] findVerticalSeam() // traverse each row, relax each pixel, check shortest sum of bottom row pixels
  
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam)
