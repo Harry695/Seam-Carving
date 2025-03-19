c4f6b5a0a108a0f537e377eeeaa33aad4d356992
Harry <oyjunhan@hotmail.com>
2025-03-18 17:26:31 -0700

    Implement tested energy() method

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
@@ -26,21 +29,40 @@ public class SeamCarver {
     }
  
     // energy of pixel at column x and row y
-    public double energy(int x, int y)
+    public double energy(int x, int y) {
+        if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) {
+            return 1000;
+        }
+        Color left = picture.get(x-1, y);
+        Color right = picture.get(x+1, y);
+        Color top = picture.get(x, y-1);
+        Color bottom = picture.get(x, y+1);
+        return Math.sqrt(
+            Math.pow(left.getRed() - right.getRed(), 2) +
+            Math.pow(left.getGreen() - right.getGreen(), 2) + 
+            Math.pow(left.getBlue() - right.getBlue(), 2) +
+            Math.pow(top.getRed() - bottom.getRed(), 2) +
+            Math.pow(top.getGreen() - bottom.getGreen(), 2) +
+            Math.pow(top.getBlue() - bottom.getBlue(), 2));
+    }
  
     // sequence of indices for horizontal seam
-    public int[] findHorizontalSeam()
+    // public int[] findHorizontalSeam()
  
     // sequence of indices for vertical seam
-    public int[] findVerticalSeam() // traverse each row, relax each pixel, check shortest sum of bottom row pixels
+    // public int[] findVerticalSeam() // traverse each row, relax each pixel, check shortest sum of bottom row pixels
  
     // remove horizontal seam from current picture
-    public void removeHorizontalSeam(int[] seam)
+    // public void removeHorizontalSeam(int[] seam)
  
     // remove vertical seam from current picture
-    public void removeVerticalSeam(int[] seam)
+    // public void removeVerticalSeam(int[] seam)
  
     //  unit testing (optional)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        SeamCarver seamCarver = new SeamCarver(new Picture("seam\\\\3x4.png"));
+        System.out.println(seamCarver.width() + " " + seamCarver.height());
+        System.out.println(seamCarver.energy(1, 2));
+    }
  
  }
